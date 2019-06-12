8a53180a0328fbf458e7f8503ccc2ee9820ed8b4
Fran <fran@Frans-iMac.local>
2019-06-12 19:57:31 +0100

    initial commit

---
+++ server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+mkfifo server.pipe # create the server pipe
+
+trap server_shut INT # call exit function when press CTRL+c
+
+# function to trap the I/O interrupt allowing clean exit
+function server_shut()
+{
+echo "...About to exit: Ctrl_c trapped"
+rm server.pipe
+exit 0
+}
+
+id=$1
+while true; do
+    read id req db table row < server.pipe # read input from server pipe
+    case $req in
+        create_database)
+            ./create_database.sh $db $table $row > "$id.pipe" & # send output to client pipe
+            ;;
+        create_table)
+            ./create_table.sh $db $table $row > "$id.pipe" &
+            ;;
+        insert)
+            ./insert.sh $db $table $row > "$id.pipe" &
+            ;;
+        query)
+            ./query.sh $db $table $row > "$id.pipe" &
+            ;;
+        shutdown)
+            rm server.pipe
+            exit 0
+            ;;
+        *)
+            echo "Error : bad request"
+            exit 1
+    esac
+done
+
